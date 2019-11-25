6c6917d80e3c1bbf40718c98d039fa0936f5dccc
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-11-25 10:48:43 -0500

    basic files structure

---
+++ routes/server.js
@@ -0,0 +1,29 @@
+//dependancys
+require("dotenv").config();
+const express = require('express');
+
+//establish our PORT and create the server
+const PORT = process.env.PORT || 3000;
+const app = express();
+//
+const db = require("./models")
+
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//require routes
+require("./routes/api-routes.js")(app);
+
+
+// sync models to database
+db.sequelize.sync().then(function() {
+
+
+    //allow server to listen for requiests
+    app.listen(PORT, function() {
+        console.log("listening on " + PORT);
+    })
+
+})
