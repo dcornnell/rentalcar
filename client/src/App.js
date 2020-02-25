c2b29cc8ab9bf325de601ff8e0a197146231ca3c
Daniel Cornnell <daniel.cornnell@gmail.com>
2020-02-25 11:39:58 -0500

    added a new car form

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import NewCar from "./components/NewCar";
 
 import "./App.css";
 import axios from "axios";
+import m from "moment";
 
 class App extends Component {
   state = { cars: [] };
@@ -15,41 +17,47 @@ class App extends Component {
   componentDidMount() {
     this.getCars();
   }
+  componentDidUpdate() {
+    this.getCars();
+  }
   render() {
     const { cars } = this.state;
     return (
-      <table className="table">
-        <thread className="align">
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Make</th>
-            <th scope="col">Model</th>
-            <th scope="col">Year</th>
-            <th scope="col">Seats</th>
-            <th scope="col">Price per day</th>
-            <th scope="col">Day Rented</th>
-            <th scope="col">Day Returned</th>
-            <th scope="col">Rented</th>
-          </tr>
-        </thread>
-        <tbody>
-          {cars.map(car => {
-            return (
-              <tr>
-                <th scope="row">{car.id}</th>
-                <td>{car.car_maker}</td>
-                <td>{car.car_model}</td>
-                <td>{car.year}</td>
-                <td>{car.seats}</td>
-                <td>{car.price}</td>
-                <td>{car.start_date}</td>
-                <td>{car.end_date}</td>
-                <td>{car.rented ? "rented" : "available"}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div>
+        <table className="table">
+          <thread className="align">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Make</th>
+              <th scope="col">Model</th>
+              <th scope="col">Year</th>
+              <th scope="col">Seats</th>
+              <th scope="col">Price per day</th>
+              <th scope="col">Day Rented</th>
+              <th scope="col">Day Returned</th>
+              <th scope="col">Rented</th>
+            </tr>
+          </thread>
+          <tbody>
+            {cars.map(car => {
+              return (
+                <tr>
+                  <th scope="row">{car.id}</th>
+                  <td>{car.make}</td>
+                  <td>{car.model}</td>
+                  <td>{car.year}</td>
+                  <td>{car.seats}</td>
+                  <td>{car.price}</td>
+                  <td>{m(car.start_date).format("MM/DD/YYYY")}</td>
+                  <td>{m(car.end_date).format("MM/DD/YYYY")}</td>
+                  <td>{car.rented ? "rented" : "available"}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <NewCar />
+      </div>
     );
   }
 }
