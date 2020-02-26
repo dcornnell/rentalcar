d7b800feb51d590c3f539b287ae0b6cf81377484
Daniel Cornnell <daniel.cornnell@gmail.com>
2020-02-26 16:03:41 -0500

    fixed an issue where heroku wasnt serving index file

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 //
 const db = require("./models");
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("client/build"));
 
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
