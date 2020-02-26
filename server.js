1a2559f563ab3b904b0c032a36e624047780a726
Daniel Cornnell <daniel.cornnell@gmail.com>
2020-02-26 16:10:30 -0500

    still not working

--- server.js
+++ server.js
@@ -10,12 +10,6 @@ const db = require("./models");
 if (process.env.NODE_ENV === "production") {
   // Exprees will serve up production assets
   app.use(express.static("client/build"));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
 }
 //middleware
 app.use(express.urlencoded({ extended: true }));
